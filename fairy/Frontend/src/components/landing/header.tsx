c3f7345a9bcfba67803b08cba7cadc62f0dc8dc9
Eric Xu <94746953+devondalebro@users.noreply.github.com>
2025-05-26 15:13:12 +1000

    center

--- fairy/Frontend/src/components/landing/header.tsx
+++ fairy/Frontend/src/components/landing/header.tsx
@@ -2,8 +2,12 @@ import { AnimatedLink } from "@/components/ui/animated-link";
 
 export default function Header({ auth }: { auth: boolean }) {
   return (
-    <header className="fixed top-10 inset-x-0 h-14 w-128 mx-auto z-50 backdrop-blur-md bg-white/75 dark:bg-black/75 rounded-full px-6 py-3 flex items-center justify-between shadow-md">
-      <span className="font-bold text-2xl tracking-tighter select-none">recruit.me</span>
+    <header className={`fixed top-10 inset-x-0 h-14 w-128 mx-auto z-50 backdrop-blur-md bg-white/75 dark:bg-black/75 rounded-full px-6 py-3 flex items-center shadow-md ${
+      auth ? "justify-between" : "justify-center"
+    }`}>
+      <span className="font-bold text-2xl tracking-tighter select-none">
+        recruit.me
+      </span>
 
       {auth && (
         <div className="flex items-center space-x-2">
